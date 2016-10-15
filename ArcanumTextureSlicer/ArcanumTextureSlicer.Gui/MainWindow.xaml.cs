f47406368a6079b1f5eb7b681fe0c7ae48897d40
Evgeniy Polyakov <evgeniy.s.polyakov@gmail.com>
2016-10-15 10:50:07 +0700

    Added open file menu item and dialog.

--- ArcanumTextureSlicer/ArcanumTextureSlicer.Gui/MainWindow.xaml.cs
+++ ArcanumTextureSlicer/ArcanumTextureSlicer.Gui/MainWindow.xaml.cs
@@ -1,28 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace ArcanumTextureSlicer.Gui
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Multiselect = false,
+                ShowReadOnly = true,
+                Filter = "Bitmap Images (*.bmp)|*.bmp|All Files (*.*)|*.*"
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+            }
+        }
+
+        private void Open_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
         }
     }
 }
